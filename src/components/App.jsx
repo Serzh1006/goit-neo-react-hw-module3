8693018d570860b2d60d32d70a8b5b1cab5f7f3b
Sergey Kravchenko <skravchenko223@gmail.com>
2025-02-15 18:54:44 +0100

    create phonebook components and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,37 @@
-// import { useState } from "react";
+import { useState } from "react";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+import data from "./baseContacts.json";
 
 const App = () => {
-  return <h1>App</h1>;
+  const [contacts, setContacts] = useState(data);
+  const [filter, setFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, newContact];
+    });
+  };
+
+  const deleteContact = (idContact) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((obj) => obj.id !== idContact);
+    });
+  };
+
+  const visibleContacts = contacts.filter((obj) =>
+    obj.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm add={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={visibleContacts} deleteContact={deleteContact} />
+    </div>
+  );
 };
 
 export default App;
